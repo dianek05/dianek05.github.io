44eb738d4b4f1815b19370074aba729a5bee83c6
Diana Lach <diana.lach@me.com>
2019-10-12 15:50:54 +0200

    Update 6-animacje.js

--- jQuery/js/6-animacje.js
+++ jQuery/js/6-animacje.js
@@ -2,14 +2,7 @@ $(function() {
     'use strict'
 });
 
-// efekt hide i show
-$('p').hide(3000).show(3000);
 
-// efekt fade in i fade out
-$('h1').fadeOut(300).fadeIn(3000);
+// moja własna animacja
+$('h3').animate({'font-size': '5em',"background-color": "red"}, 2000);
 
-// efekt slide in i slide out
-$('h2').slideUp(3000).slideOut(3000);
-
-// efekt animacji z funkcja zwortna animacji
-$('h3').animate({'font-size':"3em", "margin-left":"100px"}, 2000);
