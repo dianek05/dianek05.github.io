05a9dfcfbc5c39cb33b2441debf736dd952fe4eb
Diana Lach <diana.lach@me.com>
2019-10-12 10:43:38 +0200

    Create 1-document-ready.js

---
+++ jQuery/js/1-document-ready.js
@@ -0,0 +1,16 @@
+'use scrict';
+
+// funkcja document.ready - skrócona wersja
+
+$(function() {
+
+//  alert('test');
+
+$('#first').css({color:'red'}).hide('slow').show(2500);
+
+let second = $('#second');
+let kolekcjaParagraf = $('.paragraf');
+
+console.log(second);
+
+});
