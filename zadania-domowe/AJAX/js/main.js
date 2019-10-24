7af59649bf7a7cc272881abb25c04e7596ff30f8
Diana Lach <diana.lach@me.com>
2019-10-24 21:45:55 +0200

    Update main.js

--- zadania-domowe/AJAX/js/main.js
+++ zadania-domowe/AJAX/js/main.js
@@ -1,34 +1,17 @@
-// definicja funkcji ajax
-function ajax(metoda, url) {
-    // 1. tworzenie nowej instancji xmlhttprequest
-    let httpReq = new XMLHttpRequest();
-    // 2. otwarcie połączenia
-    httpReq.open(metoda, url);
-    // onreadystatechange - wykonuje sie za kazdym razem kiedy zmienia sie readyState
+$(function(){
+    let button = $("#button");
+    button.click(function () {
+        console.log("działa klik")
+        
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', 
+        
+        function (daneJson) {
 
-    // 3. wywołanie pętli sprawdzającej zmiane readyState
-    httpReq.onreadystatechange = function () {
-        // 4. sprawdzenie readyState
-        if (httpReq.readyState == 4) {
-            // 5. sprawdzenie statusu http
-            if (httpReq.status == 200) {
-                // 6. przypisanie danych do zmiennych
-                let returnData = httpReq.responseText; // lub (this.resposneText)
-                // 7. wykonanie funkcji na danych z wcześniejszego kroku
-                httpReq.onsuccess(returnData);
-                // 8. zamknięcie połączenia
-                httpReq = null;
-            }
+        console.log(daneJson);
+        
+        $(".display").append(`<p>ID: ${daneJson.id} <br>Name: ${daneJson.name} <br>Username: ${daneJson.username} <br>Email: ${daneJson.email}</p>`);
         }
-    }
-    // obsługa naszych danych
-    httpReq.onsuccess = function (response) {
-        // tworzę zmienna żeby sparsować response
-        let jsonObj = JSON.parse(response);
-        console.log(jsonObj.userId);
-    }
-    httpReq.send();
-}
-
-// wywołanie funkcji z parametrem
-ajax("get", "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php");
+        
+        );
+    });
+});
