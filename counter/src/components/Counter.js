c5ce1247def53f146d09f911f37cdd404307d1fc
Diana Lach <diana.lach@me.com>
2019-10-30 12:07:29 +0100

    Create Counter.js

---
+++ counter/src/components/Counter.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './Counter.css';
+import ButtonsPanel from './ButtonsPanel';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        let initValue = 0;
+        if (!isNaN(this.props.initValue)) {
+            initValue = parseInt(this.props.initValue);
+        }
+        this.state = {
+            counterValue: initValue,
+        }
+    }
+
+    changeValue = () => {
+        this.setState((prevValue) => {
+            return ({
+                counterValue: prevValue.counterValue + 1,
+            });
+        });
+    }
+
+    resetCounter = (resetCounter) => {
+        let initValue = 0;
+        if (!resetCounter) {
+            if (!isNaN(this.props.initValue)) {
+                initValue = parseInt(this.props.initValue);
+            }
+        }
+        this.setState({
+            counterValue: initValue,
+        })
+    }
+    
+    render() {
+        return (
+            <div className="counter">
+                Licznik:
+                <span className="value">{this.state.counterValue}</span>
+                <ButtonsPanel changeCounterValue={this.changeValue} resetCounterValue={this.resetCounter} />
+            </div>
+        );
+    }
+}
+
+export default Counter;
