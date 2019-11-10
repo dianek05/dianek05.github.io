a42e230f639fc7574a7c4f19780869327d7d52af
Diana Lach <diana.lach@me.com>
2019-11-10 10:23:42 +0100

    Update UserAddPanel.js

--- react-userlist/src/components/UserAddPanel.js
+++ react-userlist/src/components/UserAddPanel.js
@@ -8,24 +8,23 @@ class UsersForm extends Component {
     handleSubmit = (event) => {
         event.preventDefault(); 
 
+        let firstName = this._inputFirstName.value.trim();
+
+        if (firstName == '') {
+            console.log("Nie uzupelnione imie");
+            
+            return false;
+        }
+
+        let lastName = this._inputLastName.value.trim();
+
+        if (lastName == '') {
+            console.log("Nie uzupelnione nazwy");
+            
+            return false;
+        }
 
         this.props.addUser(this._inputFirstName.value, this._inputLastName.value); 
-        
-        this._inputFirstName.value = "";
-        this._inputLastName.value = "";
-        
-        if(this._inputFirstName.value.length < 1){
-            this.setState = () => ({
-                
-            })
-        
-        }
-        if(this._inputLastName.value.length < 1){
-            this.setState = () => ({
-                
-            })
-        
-        }
     }
 
     render(){
@@ -34,7 +33,12 @@ class UsersForm extends Component {
                 <input type="text" placeholder="First Name" className='input' ref={(data) => {this._inputFirstName = data}} ></input>&nbsp;
                 <input type="text" placeholder="Last Name" className='input' ref={(data) => {this._inputLastName = data}} ></input>
                 <button type="submit" className='btnAdd'>Add user</button>
+                {/* <ul className="errors">
+                        <il>Please give your first name</il>
+                        <il>Please give your last name</il>
+                    </ul> */}
             </form>
+                        
         )
     }
 }
